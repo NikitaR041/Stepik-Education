41fdbc66989e84c92f9843d8f8192964cd196b8e
NikitaR041 <nikitar041@gmail.com>
2025-08-01 23:15:00 +0500

    Решена вторая задача

--- Algorithms/les2/main.kt
+++ Algorithms/les2/main.kt
@@ -3,7 +3,7 @@
 Напишите рекурсивную функцию countDigits, которая вычисляет количество цифр в заданном целом числе. 
 Функция countDigits должна принимать целое число n в качестве аргумента и возвращать количество цифр в числе n.
 */
-import kotlin.math.abs
+import kotlin.math.*
 
 fun fun1(num: Int, c: Int = 0) : Int{   
     if (num == 0)  {return c}
@@ -20,3 +20,25 @@ fun fun2(num: Int, c: Int = 0) : Int {
 //     println(fun2(abs(r)))
 // }
 
+/*
+Задача 2 - OK 
+Напишите рекурсивную функцию calculatePower, которая вычисляет степень числа. 
+Функция calculatePower должна принимать два аргумента: число base и целое число exponent. 
+Она должна возвращать результат возведения числа base в степень exponent
+*/
+
+fun fun3(base: Double, exponent: Int):Double {
+    return when {
+        exponent == 0 -> 1.0
+        exponent > 0 -> base * fun3(base, exponent - 1)
+        else -> 1.0 / fun3(base, -exponent)
+    }
+}
+
+// fun main(){
+//     val (baseStr, exponentStr) = readLine()!!.split(' ')
+//     val base = baseStr.toDouble()
+//     val exponent = exponentStr.toInt()
+//     // println(fun3(base, exponent))
+//     println("%.2f".format(fun3(base, exponent))) 
+// }
